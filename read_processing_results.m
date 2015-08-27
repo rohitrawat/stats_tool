ed334b2511ae76093074f7c191da192ebd905fe9
Rohit Rawat <rohitrawat@gmail.com>
2015-08-26 22:57:39 -0500

    Fixed file reading code to handle outputs of newer versions of programs.

--- read_processing_results.m
+++ read_processing_results.m
@@ -7,7 +7,22 @@ function [y correct_class observed_class] = read_processing_results(fname)
 % http://www-ee.uta.edu/eeweb/ip/
 % Image Processing and Neural Networks Lab, UT Arlington
 
-x = dlmread(fname);
+try
+    % Read NuClass style processing results
+    x = dlmread(fname);
+catch e
+    % Read other formats, ignores all lines that don't start with a number
+    x = [];
+    fid = fopen(fname);
+    while(~feof(fid))
+        tline = fgetl(fid);
+        if(numel(tline)>0 && tline(1)>='0' && tline(1)<='9')
+            fprintf('%s\n', tline);
+            x = [x; sscanf(tline, '%f')'];
+        end
+    end
+    fclose(fid);
+end
 
 Nc = size(x,2) - 3;
 y = x(:,2:2+Nc-1);
